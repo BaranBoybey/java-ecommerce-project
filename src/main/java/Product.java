aabe9e1ac806a75a67caf91e3a487bfbd8ea8c4f
Baran <boybeybaran5@gmail.com>
2024-03-17 01:57:34 -0400

    Category package created with following classes: Category Furniture electronic skin-care

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,10 +1,18 @@
-import java.util.UUID;
+import category.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.util.UUID;
+@Getter
+@AllArgsConstructor
+@ToString
 public class Product {
     private UUID id;
     private String name;
     private double price;
     private int remainingStock;
+    private Category category;
 
 
 }
